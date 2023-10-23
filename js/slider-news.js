ab86f5320deda8ce3ce1ab8369a1e11cddf15127
01eksandr0 <139744534+01eksandr0@users.noreply.github.com>
2023-10-23 16:18:21 +0300

    slider

--- js/slider-news.js
+++ js/slider-news.js
@@ -1,3 +1,4 @@
+//slider news section
 let offset = 0;
 const slider = document.querySelector(".news-list");
 document
@@ -17,4 +18,46 @@ document
       offset = 255 + 30 + 255 + 30;
     }
     slider.style.left = -offset + "px";
+  });
+// slider pormotion section
+let prom = 0;
+const sliderProm = document.querySelector(".promotion-list");
+document
+  .querySelector(".promotion-slide-btn-second")
+  .addEventListener("click", function () {
+    prom += 255 + 30;
+    if (prom > 800) {
+      prom = -255 - 30;
+    }
+    sliderProm.style.left = -prom + "px";
+  });
+document
+  .querySelector(".promotion-slide-btn-first")
+  .addEventListener("click", function () {
+    prom -= 255 + 30;
+    if (prom < -285) {
+      prom = 255 + 30 + 255 + 30;
+    }
+    sliderProm.style.left = -prom + "px";
+  });
+//  slider partners
+let partner = 0;
+const sliderPartner = document.querySelector(".partners-list");
+document
+  .querySelector(".partner-slide-btn-second")
+  .addEventListener("click", function () {
+    partner += 255 + 30;
+    if (partner > 1200) {
+      partner = -255 - 30;
+    }
+    sliderPartner.style.left = -partner + "px";
+  });
+document
+  .querySelector(".partner-slide-btn-first")
+  .addEventListener("click", function () {
+    partner -= 255 + 30;
+    if (partner < -200) {
+      partner = 255 + 30 + 255 + 30 + 255 + 30 + 255 + 30;
+    }
+    sliderPartner.style.left = -partner + "px";
   });
